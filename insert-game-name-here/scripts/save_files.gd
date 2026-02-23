307b16e13f2ebaf8386fae4220b4e02b5fa2eade
RedRock5645 <113565541+RedRock5645@users.noreply.github.com>
2026-02-22 20:28:57 -0500

    Fixed formatting of Save Slots

--- insert-game-name-here/scripts/save_files.gd
+++ insert-game-name-here/scripts/save_files.gd
@@ -4,11 +4,17 @@ var pageNum: int = 0
 
 func _ready() -> void:
 	$saveButton.texture_normal = load(SaveLoad.getThumbnail(0+(6*pageNum)))
+	$saveButton/Label.text = "Save File "+str(1+(6*pageNum))
 	$saveButton2.texture_normal = load(SaveLoad.getThumbnail(1+(6*pageNum)))
+	$saveButton2/Label.text = "Save File "+str(2+(6*pageNum))
 	$saveButton3.texture_normal = load(SaveLoad.getThumbnail(2+(6*pageNum)))
+	$saveButton3/Label.text = "Save File "+str(3+(6*pageNum))
 	$saveButton4.texture_normal = load(SaveLoad.getThumbnail(3+(6*pageNum)))
+	$saveButton4/Label.text = "Save File "+str(4+(6*pageNum))
 	$saveButton5.texture_normal = load(SaveLoad.getThumbnail(4+(6*pageNum)))
+	$saveButton5/Label.text = "Save File "+str(5+(6*pageNum))
 	$saveButton6.texture_normal = load(SaveLoad.getThumbnail(5+(6*pageNum)))
+	$saveButton6/Label.text = "Save File "+str(6+(6*pageNum))
 func _on_save_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/background.tscn")
 	SaveLoad._load(0+(6*pageNum))
@@ -37,12 +43,18 @@ func _on_save_button_6_pressed() -> void:
 	SaveLoad._load(5+(6*pageNum))
 
 func reset_save_text()->void:
-	$saveButton.text = "Save File "+ str(1+6*pageNum)
-	$saveButton2.text = "Save File "+ str(2+6*pageNum)
-	$saveButton3.text = "Save File "+ str(3+6*pageNum)
-	$saveButton4.text = "Save File "+ str(4+6*pageNum)
-	$saveButton5.text = "Save File "+ str(5+6*pageNum)
-	$saveButton6.text = "Save File "+ str(6+6*pageNum)
+	$saveButton.texture_normal = load(SaveLoad.getThumbnail(0+(6*pageNum)))
+	$saveButton/Label.text = "Save File "+str(1+(6*pageNum))
+	$saveButton2.texture_normal = load(SaveLoad.getThumbnail(1+(6*pageNum)))
+	$saveButton2/Label.text = "Save File "+str(2+(6*pageNum))
+	$saveButton3.texture_normal = load(SaveLoad.getThumbnail(2+(6*pageNum)))
+	$saveButton3/Label.text = "Save File "+str(3+(6*pageNum))
+	$saveButton4.texture_normal = load(SaveLoad.getThumbnail(3+(6*pageNum)))
+	$saveButton4/Label.text = "Save File "+str(4+(6*pageNum))
+	$saveButton5.texture_normal = load(SaveLoad.getThumbnail(4+(6*pageNum)))
+	$saveButton5/Label.text = "Save File "+str(5+(6*pageNum))
+	$saveButton6.texture_normal = load(SaveLoad.getThumbnail(5+(6*pageNum)))
+	$saveButton6/Label.text = "Save File "+str(6+(6*pageNum))
 
 func _on_pre_page_btn_pressed() -> void:
 	if pageNum>0:
