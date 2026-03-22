e7622e0010501a938d1436cb4a9bc445126c9c54
RedRock5645 <113565541+RedRock5645@users.noreply.github.com>
2026-03-21 20:21:28 -0400

    Save system works! You can choose which save file to save to

--- insert-game-name-here/scripts/Save-related/saveLoad.gd
+++ insert-game-name-here/scripts/Save-related/saveLoad.gd
@@ -1,18 +1,34 @@
 #class_name Saver
 extends Control
 #Handles saving the game
-const SAVE_PATH = "user://save/save"
+const SAVE_PATH = "res://save/save"
 const IMG_PATH = "res://save/thumbnail"
+const VIEWPORT_WIDTH = 1920
+const VIEWPORT_HEIGHT = 1080
+
+var img
 var saveFileNum:int = 0;
+
+var load_mode = true
+#checks if player is clicking on thumbnail to save or to load
+#if true then load, if false then save
+
 
 var saveFileData:SaveData = SaveData.new()
 var dialogIndex:int
+func open_save():
+	img =  get_viewport().get_texture().get_image()
+	img.resize(120, 80) # That's the size of the thumbnails for the save files
+	load_mode = false
+	get_tree().change_scene_to_file("res://scenes/Save-menu-related/save_files.tscn")
+	get_tree().reload_current_scene()
+	
+
+func change_save_num(save_num:int):
+	saveFileNum = save_num
 func _save():
-	var img =  get_viewport().get_texture().get_image()
-	img.resize(100,100,1)
 	img.save_png(IMG_PATH+str(saveFileNum)+".png")
 	ResourceSaver.save(saveFileData, SAVE_PATH+str(saveFileNum)+".tres")
-	print(saveFileData.dialogIndex)
 
 func _load(fileNum):
 	if FileAccess.file_exists(SAVE_PATH+str(fileNum)+".tres"):
@@ -28,5 +44,6 @@ func getThumbnail(saveNum)->String:
 	return file_path;
 func _input(event):
 	if event.is_action_pressed("Shoot"):
-		_save()
-		
+		open_save()
+	if event.is_action_pressed("menu"):
+		get_tree().change_scene_to_file("res://scenes/Save-menu-related/main_menu.tscn")
--- insert-game-name-here/scripts/Save-related/save_files.gd
+++ insert-game-name-here/scripts/Save-related/save_files.gd
@@ -2,45 +2,47 @@ extends Node2D
 #pageNum starts at zero
 var pageNum: int = 0
 
+
+
 func _ready() -> void:
-	$saveButton.texture_normal = load(SaveLoad.getThumbnail(0+(6*pageNum)))
-	$saveButton/Label.text = "Save File "+str(1+(6*pageNum))
-	$saveButton2.texture_normal = load(SaveLoad.getThumbnail(1+(6*pageNum)))
-	$saveButton2/Label.text = "Save File "+str(2+(6*pageNum))
-	$saveButton3.texture_normal = load(SaveLoad.getThumbnail(2+(6*pageNum)))
-	$saveButton3/Label.text = "Save File "+str(3+(6*pageNum))
-	$saveButton4.texture_normal = load(SaveLoad.getThumbnail(3+(6*pageNum)))
-	$saveButton4/Label.text = "Save File "+str(4+(6*pageNum))
-	$saveButton5.texture_normal = load(SaveLoad.getThumbnail(4+(6*pageNum)))
-	$saveButton5/Label.text = "Save File "+str(5+(6*pageNum))
-	$saveButton6.texture_normal = load(SaveLoad.getThumbnail(5+(6*pageNum)))
-	$saveButton6/Label.text = "Save File "+str(6+(6*pageNum))
+	reset_save_text()
+
+
+#Do the following lines of code seem stupid? Yup! but I dunno how else to do this and it works so....
 func _on_save_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/VN-related/background.tscn")
-	SaveLoad._load(0+(6*pageNum))
+	print("1~!")
+	save_btn_functions(0)
+	reset_save_text()
+
 
 
 func _on_save_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/VN-related/background.tscn")
-	SaveLoad._load(1+(6*pageNum))
+	print("2~!")
+	save_btn_functions(1)
+	reset_save_text()
 
 func _on_save_button_3_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/VN-related/background.tscn")
-	SaveLoad._load(2+(6*pageNum))
+	save_btn_functions(2)
 
 func _on_save_button_4_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/VN-related/background.tscn")
-	SaveLoad._load(3+(6*pageNum))
+	save_btn_functions(3)
 
 
 func _on_save_button_5_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/VN-related/background.tscn")
-	SaveLoad._load(4+(6*pageNum))
+	save_btn_functions(4)
 
 
 func _on_save_button_6_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/VN-related/background.tscn")
-	SaveLoad._load(5+(6*pageNum))
+	save_btn_functions(5)
+
+func save_btn_functions(index:int)->void:
+	if SaveLoad.load_mode:
+		get_tree().change_scene_to_file("res://scenes/VN-related/background.tscn")
+		SaveLoad._load(index+(6*pageNum))
+	else:
+		SaveLoad.change_save_num(index)
+		SaveLoad._save()
+		SaveLoad.load_mode = true
 
 func reset_save_text()->void:
 	$saveButton.texture_normal = load(SaveLoad.getThumbnail(0+(6*pageNum)))
