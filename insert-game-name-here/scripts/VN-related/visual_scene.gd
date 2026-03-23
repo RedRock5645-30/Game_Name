f55c5c086a22e2a8cb070f63bb3f608775b51d78
RedRock5645 <113565541+RedRock5645@users.noreply.github.com>
2026-03-22 20:00:16 -0400

    Moved script into a json file

--- insert-game-name-here/scripts/VN-related/visual_scene.gd
+++ insert-game-name-here/scripts/VN-related/visual_scene.gd
@@ -3,16 +3,7 @@ extends Node2D
 @onready var dialogUi = $CanvasLayer2/DialogueUI
 
 
-const dialogLines : Array[String] = [
-	"Chunni: Time to face the Fortress of Generic High",
-	"Chunni: Wading through the sea of corporeal figures and metal towers, I slowly approach the fated chamber enumerated with 113, the cage in which I am destined to spend my foreseeable days. A–",
-	"???: Ah! You must be the new student. I’m Mr. Sensei.",
-	"Chunni: Tch. What a loser.",
-	"Chunni: Huh, that's an interesting name…",
-	"Noelle:...Oopsie! Well, I’m here now aren’t I?",
-	"Matty: N-no, what? Invisibility potion ?...No it's just that I’m wearing my…"
-	
-]
+var dialogLines : Array= []
 
 # Called when the node enters the scene tree for the first time.
 #okay so saveLoad is basically the save file so anything saveLoad.VARIABLE_NAME is stuff that needs to be saved
@@ -21,8 +12,20 @@ func _ready() -> void:
 	#if SaveLoad.saveFileData.dialogIndex == TYPE_NIL:
 		#for if smth bad happens it starts from beginning
 		#SaveLoad.saveFileData.dialogIndex = 0
+	dialogLines = load_dialogue("res://scripts/Story/chapter1.json")
+	
 	processCurLine()
-
+func load_dialogue(file_path):
+	#Check if file exists
+	if not FileAccess.file_exists(file_path):
+		print(file_path+" does not exist")
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	var content = file.get_as_text()
+	#parse json format into text
+	var json_content = JSON.parse_string(content)
+	#returns the dialogue
+	return json_content
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -47,7 +50,8 @@ func parseLine(line: String):
 		"speakerName": lineInfo[0],
 		"dialogLine": lineInfo[1]
 	}
-	
+
+#Switches out the character and speaker text to match character talking
 func processCurLine():
 	var lineInfo = parseLine(dialogLines[SaveLoad.saveFileData.dialogIndex])
 	dialogUi.changeLine(lineInfo["speakerName"], lineInfo["dialogLine"])
